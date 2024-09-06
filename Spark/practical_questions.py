e6d2a3081d013d2e433f559589d6fb6e4d532b9d
shivamnahar1196 <shivam.projects.nahar@gmail.com>
2024-09-06 12:44:04 +0530

    Update practical_questions.py

--- Spark/practical_questions.py
+++ Spark/practical_questions.py
@@ -1,3 +1,6 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('test').getOrCreate()
+
 # TODO 1: Write PySpark Code to read Text file having JSON data and get the output as per below:
 """
 Text File :
@@ -23,11 +26,8 @@ id, name, city
 2, aditya, city2
 """
 
-from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, StructField, StructType, ArrayType, IntegerType
 from pyspark.sql.functions import explode
-
-spark = SparkSession.builder.appName('test').getOrCreate()
 
 schema = StructType([
     StructField('id', StringType()),
@@ -42,6 +42,8 @@ df2 = df1.withColumn('city', explode('city'))
 df2.show(truncate=False)
 
 """
+o/p: 
+
 +---+------+---------------------+
 |id |name  |city                 |
 +---+------+---------------------+
@@ -59,5 +61,37 @@ df2.show(truncate=False)
 |2  |aditya|city2|
 """
 
+# TODO 2: Write query for below Output
+"""
+Input :
+col_code
+red
+blue
+green
 
+Output :
+col1,col2
+red, blue
+red,green
+blue,green
+"""
+from pyspark.sql.functions import col
+
+data = [('red',), ('blue',), ('green',)]
+df = spark.createDataFrame(data, ['col_code'])
+
+ndf = df.alias("df1").join(df.alias("df2"), col("df1.col_code") < col("df2.col_code"))
+df.createOrReplaceTempView('ndf')
+ndf.show()
+
+"""
+o/p:
++--------+--------+
+|col_code|col_code|
++--------+--------+
+|    blue|     red|
+|   green|     red|
+|    blue|   green|
++--------+--------+
+"""
 
